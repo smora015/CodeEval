6d38d97931af6324aab47571f3f07e390fd0bee6
Sergio Morales <sergio.bcoe@gmail.com>
2013-12-20 11:03:23 -0800

    Update multlists.py

--- multlists.py
+++ multlists.py
@@ -1,12 +1,22 @@
+"""
+    (c) Sergio Morales
+    CodeEval Challenge: Multiply Lists
+    Date Solved: 12/20/13
+"""
+
+#!/usr/local/bin/python3
+
 import sys
+
 test_cases = open(sys.argv[1], 'r')
 for test in test_cases:
-    list1 = test[0:test.find("|")-1]
-    list2 = test[test.find("|")+1:]
+    list1 = test[0:test.find("|")-1].split(" ")
+    list2 = test[test.find("|")+2:len(test)-1].split(" ")
+    
     list1 = [int(x) for x in list1]
     list2 = [int(x1) for x1 in list2]
     
-    for i in range(0,len(list1)-1):
-        print(list1[i]*list2[i]+" ", end="")
+    for i in range(0,len(list1)):
+        print(str(list1[i]*list2[i])+" ", end="")
     print("", end="\n")
 test_cases.close()
